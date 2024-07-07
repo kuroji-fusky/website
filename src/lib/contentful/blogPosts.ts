152838d794c0f90e88e84d47b40981013fb7f402
Kerby Keith Aquino <hello@kurojifusky.com>
2024-07-07 14:19:03 +0800

    rendering individual blog posts done (kinda)

--- src/lib/contentful/blogPosts.ts
+++ src/lib/contentful/blogPosts.ts
@@ -6,6 +6,11 @@ interface FetchBlogContent {
   category?: string
 }
 
+const sortInAscendingOrder = <T extends object>(arr: T[]) => {
+  // @ts-expect-error
+  return arr.sort((a, b) => (new Date(b.date) as any) - (new Date(a.date) as any))
+}
+
 const fetchBlogContents = async ({ limit, category }: FetchBlogContent) => {
   return await client.getEntries<BlogPostContent>({
     content_type: "blogPost",
@@ -17,7 +22,7 @@ const fetchBlogContents = async ({ limit, category }: FetchBlogContent) => {
 export const latestBlogPosts = async ({ limit }: FetchBlogContent) => {
   const c = await fetchBlogContents({ limit })
 
-  return c.items.map((item) => {
+  const posts = c.items.map((item) => {
     const { banner, category, content, description, overridePublishDate, slug, title } = item.fields
     const image = banner ? `https:${banner.fields.file.url}?fm=webp&q=75&w=854&h=480` : ""
 
@@ -34,12 +39,10 @@ export const latestBlogPosts = async ({ limit }: FetchBlogContent) => {
       _tags: item.metadata.tags
     }
   })
+
+  return sortInAscendingOrder(posts)
 }
 
 export const blogpostsByCategory = async ({ limit, category }: FetchBlogContent) => {
   const c = await fetchBlogContents({ limit, category })
-}
-
-export const fetchCurrentPage = async ({}: any) => {
-  const c = await fetchBlogContents({})
 }
