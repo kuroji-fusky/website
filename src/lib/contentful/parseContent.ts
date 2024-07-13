556a4b6164999d42093086d142d7e6e1071c76c9
Kerby Keith Aquino <hello@kurojifusky.com>
2024-07-13 18:25:00 +0800

    sanitize that shit

--- src/lib/contentful/parseContent.ts
+++ src/lib/contentful/parseContent.ts
@@ -2,42 +2,89 @@ import type { Options } from "@contentful/rich-text-html-renderer"
 import { BLOCKS, MARKS, INLINES } from "@contentful/rich-text-types"
 import type { CustomInlineEntry } from "./types"
 
+/**
+ * Writing stringed HTML is gross, so I made this so it's less error-prone lol
+ *
+ * @param tag A vaild HTML tag
+ * @param content Anything that'll be appended from `textContent`
+ * @param attrs Attributes from the HTML element you provided
+ * @returns A gorgeous HTML string that's safe to parse for Contentful to eat :3
+ */
+const sanitizedHTML = <T extends keyof HTMLElementTagNameMap, C = string>(
+  tag: T,
+  content?: C,
+  attrs?: Partial<Omit<HTMLElementTagNameMap[T], keyof HTMLElementTagNameMap[T]>>
+) => {
+  // `document` and `window` is not available for server so I had to deal cobbling with fukin arrays
+  const START_TAG = `<${tag}>`
+  const END_TAG = `</${tag}>`
+
+  const START_TAG_SC = `<${tag} `
+  const END_TAG_SC = ` />`
+
+  const parsedAttributes = attrs
+    ? Object.entries(attrs)
+        .map(([k, v]) => `${k}="${v}"`)
+        .join(" ")
+    : ""
+
+  if (tag === "img") return [START_TAG_SC, parsedAttributes, END_TAG_SC].join("")
+  if (parsedAttributes) return [START_TAG_SC, parsedAttributes, ">", content, END_TAG].join("")
+
+  return [START_TAG, content, END_TAG].join("")
+}
+
 export const rendererOptions: Partial<Options> = {
   renderMark: {
-    [MARKS.BOLD]: (t) => `<strong>${t}</strong>`,
-    [MARKS.ITALIC]: (t) => `<em>${t}</em>`,
+    [MARKS.BOLD]: (t) => sanitizedHTML("strong", t),
+    [MARKS.ITALIC]: (t) => sanitizedHTML("em", t),
     [MARKS.CODE]: (t) =>
-      `<code class="rounded-md bg-kuro-lavender-900 text-base py-0.5 px-1">${t}</code>`
+      sanitizedHTML("code", t, { class: "rounded-md bg-kuro-lavender-900 text-base py-0.5 px-1" })
   },
   renderNode: {
     [BLOCKS.QUOTE]: (node, next) =>
-      `<blockquote class="px-6 py-5 border-l-[6px] border-kuro-lavender-300 bg-kuro-lavender-900 bg-opacity-40 my-2.5 text-kuro-violet-50 prose-p:!font-medium">${next(node.content)}</blockquote>`,
+      sanitizedHTML("blockquote", next(node.content), {
+        class:
+          "px-6 py-5 border-l-[6px] border-kuro-lavender-300 bg-kuro-lavender-900 bg-opacity-40 my-2.5 text-kuro-violet-50 prose-p:!font-medium"
+      }),
     [BLOCKS.PARAGRAPH]: (node, next) => {
       const nodeContent = node.content
-      if (!nodeContent.some((x) => (x as any).value === "")) return `<p>${next(nodeContent)}</p>`
+      if (!nodeContent.some((x) => (x as any).value === ""))
+        return sanitizedHTML("p", next(nodeContent))
 
-      return `<div>${next(nodeContent)}</div>`
+      return sanitizedHTML("div", next(nodeContent))
     },
     [BLOCKS.EMBEDDED_ASSET]: (node) => {
       const embedFields = node.data.target.fields
 
-      return `<img src="https:${embedFields.file.url}" alt="${embedFields.title}" class="rounded-md h-[32rem] mx-auto" />`
+      return sanitizedHTML("img", null, {
+        src: `https:${embedFields.file.url}`,
+        alt: "${embedFields.title}",
+        class: "rounded-md h-[32rem] mx-auto"
+      })
     },
     [INLINES.EMBEDDED_ENTRY]: (node) => {
       const { sys, fields } = node.data.target as CustomInlineEntry
       const strippedUrl = fields.url?.split("/").at(-1)
 
       if (sys.contentType.sys.id === "youTubeVideo") {
-        return `
-        <div class="grid place-items-center">
-          <iframe class="w-full h-full aspect-video mx-auto rounded-md overflow-hidden block" src="https://www.youtube-nocookie.com/embed/${strippedUrl}" frameborder="0" allow="clipboard-write; encrypted-media; web-share"></iframe>
-        </div>`
+        return sanitizedHTML("iframe", null, {
+          src: `https://www.youtube-nocookie.com/embed/${strippedUrl}`,
+          allow: "clipboard-write; encrypted-media; web-share;",
+          class: "w-full h-full aspect-video mx-auto rounded-md overflow-hidden block",
+          frameborder: "0"
+        })
       }
 
-      return `<div>${node}</div>`
+      return sanitizedHTML("div", node)
     },
     [INLINES.HYPERLINK]: (node, next) =>
-      `<a class="text-kuro-lavender-300 hover:text-kuro-lavender-400 hover:underline" href="${node.data.uri}" target="_blank" rel="noopenner noreferrer">${next(node.content)}</a>`
+      sanitizedHTML("a", next(node.content), {
+        class: "text-kuro-lavender-300 hover:text-kuro-lavender-400 hover:underline",
+        href: node.data.uri,
+        target: "_blank",
+        rel: "noopenner noreferrer"
+      })
   }
 }
 
