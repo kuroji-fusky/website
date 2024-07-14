f64fdbbf94ddaa15bd70e6de21604e5048e78871
Kerby Keith Aquino <hello@kurojifusky.com>
2024-07-14 19:25:41 +0800

    add deps for lit, svelte, and rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig, squooshImageService } from "astro/config"
-
 import autoprefixer from "autoprefixer"
-
 import tailwind from "@astrojs/tailwind"
 import vue from "@astrojs/vue"
 import sitemap from "@astrojs/sitemap"
 import mdx from "@astrojs/mdx"
 import vercel from "@astrojs/vercel/serverless"
+import lit from "@astrojs/lit"
+import svelte from "@astrojs/svelte"
 
+// https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   adapter: vercel({
@@ -19,11 +20,13 @@ export default defineConfig({
     prefetchAll: true
   },
   integrations: [
+    lit(),
+    svelte(),
+    tailwind(),
     vue({
       customElement: true
     }),
     mdx(),
-    tailwind(),
     sitemap()
   ],
   vite: {
