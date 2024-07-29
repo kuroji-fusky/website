f5c7f33e667571c90127b435a448832ae4b67338
Kerby Keith Aquino <hello@kurojifusky.com>
2024-07-30 06:39:55 +0800

    add enum validators

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,22 @@
 import { z, defineCollection } from "astro:content"
 
+const showcaseType = z.enum(["Browser extension", "Web application"])
+
+const showcaseTechnologies = z.enum([
+  "nextjs",
+  "sveltekit",
+  "astro",
+  "plasmo",
+  "tauri",
+  "flutter",
+  "mongodb",
+  "redis"
+])
+const showcaseInfras = z.enum(["vercel", "digitalocean", "docker", "aws", "gcp"])
+
+export type ShowcaseTechnologiesLiteral = z.infer<typeof showcaseTechnologies>
+export type ShowcaseInfrasLiteral = z.infer<typeof showcaseInfras>
+
 export const collections = {
   showcase: defineCollection({
     type: "content",
@@ -7,12 +24,12 @@ export const collections = {
       title: z.string(),
       repo: z.string(),
       image: z.string(),
-      type: z.string(),
+      type: showcaseType,
       date: z.string().transform((s) => new Date(s)),
       excerpt: z.string(),
       details: z.object({
-        technologies: z.array(z.string()).nullable(),
-        infrastructure: z.array(z.string()).nullable()
+        technologies: z.array(showcaseTechnologies).nullable(),
+        infrastructure: z.array(showcaseInfras).nullable()
       })
     })
   })
