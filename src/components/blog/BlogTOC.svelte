41e71d2c89bb4f3f34be3069cb1ef1cd95be9ce9
Kerby Keith Aquino <hello@kurojifusky.com>
2024-09-11 01:47:51 +0800

    toc wrap

--- src/components/blog/BlogTOC.svelte
+++ src/components/blog/BlogTOC.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 </script>
 
-<div>
-  <slot />
-</div>
+<aside
+  class="bg-kuro-dark2 flex-shrink-0 md:flex [align-self:start] hidden md:w-56 lg:w-[16.75rem] sticky top-20 md:ml-5 lg:ml-10"
+>
+  <div class="w-full">
+    <div class="mb-2 font-bold text-xl">Table of contents</div>
+    <slot />
+  </div>
+</aside>
