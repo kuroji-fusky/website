2825d444eb1215b4d61b9f805313b1ecf460e604
Kerby Keith Aquino <hello@kurojifusky.com>
2025-03-03 10:30:45 +0800

    cleaning in progress

--- apps/kurojifusky.com/src/components/Navbar/Navbar.stores.ts
+++
@@ -1,6 +0,0 @@
-import { togglerStore } from "$stores/togglerStore"
-
-export const isMobileNavOpen = togglerStore(false)
-
-// TODO add a modal state to track with from other states too
-export const isSearchActive = togglerStore(false)
